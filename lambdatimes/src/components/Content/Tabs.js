eb0490b48bd9e5fe80d4655fc4c0535d9316f6ae
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-18 09:49:52 -0700

    Add tab functionality and display appropriate cards

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +10,13 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, index) => <Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} key={tab+index} />)}
       </div>
     </div>
   );
 };
+
+
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
