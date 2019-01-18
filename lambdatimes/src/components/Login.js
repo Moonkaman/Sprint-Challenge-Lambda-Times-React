38edebe6767942f5581de6596f121f71ad98d1ec
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-18 12:11:37 -0700

    Style change on login button

--- lambdatimes/src/components/Login.js
+++ lambdatimes/src/components/Login.js
@@ -22,7 +22,7 @@ const LoginButton = styled.button`
   border: none;
   color: white;
   font-size: 18px;
-  padding: 3px 0;
+  padding: 5px 0;
   border-top-right-radius: 4px;
   border-bottom-right-radius: 4px;
   cursor: pointer;
