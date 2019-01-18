dc69957f7113c81b67b6ec3c6bd104fe22669eff
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-18 11:24:20 -0700

    Added log in styles

--- lambdatimes/src/components/Login.js
+++ lambdatimes/src/components/Login.js
@@ -1,4 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoginForm = styled.form`
+
+`
+
+const LoginInput = styled.input`
+  outline: none;
+  border: none;
+  padding: 5px 0 5px 5px;
+  font-size: 18px;
+  width: 80%;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`
+
+const LoginButton = styled.button`
+  width: 20%;
+  background: #304FFE;
+  outline: none;
+  border: none;
+  color: white;
+  font-size: 18px;
+  padding: 3px 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  cursor: pointer;
+  transition: background 0.2s ease;
+  &:hover {
+    background: #2979FF;
+  }
+`
 
 class Login extends React.Component {
   constructor() {
@@ -20,11 +52,11 @@ class Login extends React.Component {
 
   render(){
     return (
-      <form action="">
+      <LoginForm action="">
         <h1>Please enter a Username</h1>
-        <input type="text" placeholder='Username...' value={this.state.usernameInput} onChange={this.handleInput} />
-        <button onClick={this.logIn}>Log In</button>
-      </form>
+        <LoginInput type="text" placeholder='Username...' value={this.state.usernameInput} onChange={this.handleInput} />
+        <LoginButton onClick={this.logIn}>Log In</LoginButton>
+      </LoginForm>
     )
   }
 }
