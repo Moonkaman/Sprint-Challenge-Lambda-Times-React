3eb93cbe24fd96736fd9f13815e9ac43fd36df93
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-18 09:57:13 -0700

    Add proptypes to card and cards js files

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Card from './Card';
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
       {props.cards.map((card, index) => <Card card={card} key={card.author+index} />)}
     </div>
   )
 }
 
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+  }))
+}
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
